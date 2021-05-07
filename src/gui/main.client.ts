726218624d88f81ec6b700b932c0ec6fbba44bea
beltsazars <tomflynnbeltsazar@gmail.com>
2021-05-07 15:11:09 +0700

    Delete unused files

--- src/gui/main.client.ts
+++
@@ -1,9 +0,0 @@
-export {};
-
-let starterGuiService = game.GetService("StarterGui")
-starterGuiService.SetCoreGuiEnabled(Enum.CoreGuiType.Health, false)
-starterGuiService.SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
-
-// const screenGui = script.Parent?.WaitForChild("ScreenGui") as ScreenGui
-// const textButton = screenGui?.WaitForChild("TextButton") as TextButton
-// textButton.MouseButton1Click.Connect(() => print("clicked"))
